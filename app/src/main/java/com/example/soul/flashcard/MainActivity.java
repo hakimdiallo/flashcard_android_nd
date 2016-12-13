0a863671cfa59ce78d42bb9c6bd530b58a49615f
DIALLO souleymane <diallos@informatique.univ-paris-diderot.fr>
2016-12-13 14:17:51 +0100

    adding the main activity

--- app/src/main/java/com/example/soul/flashcard/MainActivity.java
+++ app/src/main/java/com/example/soul/flashcard/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.soul.flashcard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button add;
+    private Button create;
+    private Button delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add = (Button) findViewById(R.id.buttonAdd);
+        create = (Button) findViewById(R.id.buttonCreate);
+        delete = (Button) findViewById(R.id.buttonDelete);
+    }
+
+    public void add(View view){
+        Intent intent = new Intent();
+    }
+
+    public void create(View view){
+        Intent intent = new Intent();
+    }
+
+    public void delete(View view){
+        Intent intent = new Intent();
     }
 }

2f74c7f0372b734dbbe779a8231b2f966092c6c3
DIALLO souleymane <diallos@informatique.univ-paris-diderot.fr>
2016-12-22 15:28:02 +0100

    Add card done and amelioration add new jeu

--- app/src/main/java/com/example/soul/flashcard/AddNewCardActivity.java
+++ app/src/main/java/com/example/soul/flashcard/AddNewCardActivity.java
@@ -3,16 +3,109 @@ package com.example.soul.flashcard;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class AddNewCardActivity extends AppCompatActivity {
+    private EditText question, reponse, prio;
+    private boolean q, r, p;
+    private Button save;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_card);
+
+        save = (Button) findViewById(R.id.save);
+        save.setEnabled(false);
+
+        q = r = p = false;
+
+        question = (EditText) findViewById(R.id.question);
+        question.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                q = false;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!s.toString().isEmpty()){
+                    q = true;
+                }
+                if(q && r && p){
+                    save.setEnabled(true);
+                }
+            }
+        });
+        reponse = (EditText) findViewById(R.id.reponse);
+        reponse.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                r = false;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!s.toString().isEmpty()){
+                    r = true;
+                }
+                if(q && r && p){
+                    save.setEnabled(true);
+                }
+            }
+        });
+        prio = (EditText) findViewById(R.id.prio);
+        prio.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                p = false;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!s.toString().isEmpty()){
+                    p = true;
+                }
+                if(q && r && p){
+                    save.setEnabled(true);
+                }
+            }
+        });
+
+    }
+
+    public void save(View view){
+        InterfaceFlashProvider fp = new InterfaceFlashProvider(this);
+        String qstr = question.getText().toString();
+        String rstr = reponse.getText().toString();
+        int pr = Integer.parseInt(prio.getText().toString());
         Intent intent = getIntent();
         String nom = intent.getStringExtra("nom");
-        Toast.makeText(AddNewCardActivity.this,nom,Toast.LENGTH_LONG).show();
+        if (fp.insertIntoCarte(nom,qstr,rstr,pr)){
+            Toast.makeText(this,"La carte a bien été enregitrée.",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this,"Erreur lors de l'enregistrement. Veuillez réessayer.",Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/example/soul/flashcard/NewGame.java
+++ app/src/main/java/com/example/soul/flashcard/NewGame.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,12 +22,34 @@ public class NewGame extends AppCompatActivity {
     public EditText name_t;
     public TextView res;
     private String name;
+    private Button create;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newgame);
-        Intent intent = getIntent();
+
+        create = (Button) findViewById(R.id.create);
+        create.setEnabled(false);
+
         name_t = (EditText) findViewById(R.id.name);
+        name_t.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                create.setEnabled(false);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!s.toString().isEmpty())
+                    create.setEnabled(true);
+            }
+        });
         res = (TextView) findViewById(R.id.result);
     }
 
@@ -33,10 +58,10 @@ public class NewGame extends AppCompatActivity {
         InterfaceFlashProvider ip =new InterfaceFlashProvider(this);
         boolean check= ip.insertIntoJeux(name);
         if(check){
-            res.setText("Le sujet est bien ajouté!!!");
+            res.setText("Le jeu a bien été ajouté!!!");
         }
         else {
-            res.setText("Sujet existed déjà!!!");
+            res.setText("le jeu existe déjà!!!");
         }
     }
 }

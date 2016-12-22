106172d0c60973a2ba7435086ab61e41ab6b8ece
DIALLO souleymane <diallos@informatique.univ-paris-diderot.fr>
2016-12-22 16:16:21 +0100

    amelioration de la suppression

--- app/src/main/java/com/example/soul/flashcard/ShowGameToAddCardActivity.java
+++ app/src/main/java/com/example/soul/flashcard/ShowGameToAddCardActivity.java
@@ -1,7 +1,9 @@
 package com.example.soul.flashcard;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +21,7 @@ import java.util.NoSuchElementException;
 public class ShowGameToAddCardActivity extends AppCompatActivity {
     private boolean options;
     InterfaceFlashProvider fp;
+    private String nom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,14 +44,31 @@ public class ShowGameToAddCardActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String nom = adapter.getItem(position);
+                nom = adapter.getItem(position);
                 if (options){
                     Intent in = new Intent(ShowGameToAddCardActivity.this,AddNewCardActivity.class);
                     in.putExtra("nom",nom);
                     startActivity(in);
                 }else{
-                    fp.deleteJeux(nom);
-                    adapter.remove(nom);
+                    new AlertDialog.Builder(ShowGameToAddCardActivity.this)
+                            .setTitle(R.string.titleDelete)
+                            .setMessage(R.string.messageDelete)
+                            .setPositiveButton(R.string.oui, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    fp.deleteJeux(nom);
+                                    adapter.remove(nom);
+                                    Toast.makeText(ShowGameToAddCardActivity.this,R.string.deleted,Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .setNegativeButton(R.string.non, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(ShowGameToAddCardActivity.this,R.string.cancelDelete,Toast.LENGTH_LONG).show();
+                                }
+                    })
+                    .show();
+
                 }
             }
         });
